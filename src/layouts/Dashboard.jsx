38a0303d1d14fd2747aa76461f66c19ae7cc144e
Nafibal <baliya363@gmail.com>
2023-05-16 12:57:09 +0700

    feat : finished dashboard layout UI

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,11 +3,17 @@ import Sidebar from "../components/layouts/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="bg-primary ps-12 flex flex-col justify-center w-full h-24">
-        <h1 className="text-white text-2xl">Dashboard</h1>
-        <h2 className="text-white">Subtitle</h2>
+    <div className="flex relative">
+      <Sidebar expand={false} className="z-10" />
+      <div className=" absolute left-40 right-0 ">
+        <div className="bg-primary ps-12 flex flex-col justify-center h-24 -z-10">
+          <h1 className="text-white text-2xl">Dashboard</h1>
+          <h2 className="text-white">Subtitle</h2>
+        </div>
+        <div>
+          {/* Buat Outlet dari React Router */}
+          {/* <Outlet/> */}
+        </div>
       </div>
     </div>
   );
