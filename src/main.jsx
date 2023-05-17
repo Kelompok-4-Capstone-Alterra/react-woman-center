0eb1a2b2ca19326be6759411b3d337f1d14fbe37
unknown <wiraprathamaalvin@gmail.com>
2023-05-17 17:31:36 +0800

    feat: finished login ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Dashboard from "./layouts/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <Login />
   </React.StrictMode>
 );
