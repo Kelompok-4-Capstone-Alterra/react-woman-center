a35605e670ec8e0355e4ca19ec4a27d765538e61
unknown <wiraprathamaalvin@gmail.com>
2023-05-31 17:04:59 +0800

    chores: create dropdown component

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,10 +1,11 @@
 import React , { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import InputField from './InputField'
+import Dropdown from './Dropdown'
 
 
 const FormLogin = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, control, formState: { errors } } = useForm();
 
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
@@ -15,7 +16,17 @@ const FormLogin = () => {
      <form onSubmit={handleSubmit(onSubmit)}>
         <InputField name="username" label="Username" type="text" placeholder="johndoe" errors={errors} register={register}  />
         <InputField name="password" label="Password" type="password" placeholder="it's secret" errors={errors} register={register}  />
-        <input
+        <Dropdown
+          control={control}
+          name="myDropdown"
+          label="Choose Sub-Menu"
+          placeholder="Choose"
+        >
+          <option label="option 1" value="option1"/>
+          <option label="option 2" value="option2"/>
+          <option label="option 3" value="option3"/>
+        </Dropdown>
+      <input
         className="flex justify-center bg-primaryMain px-2 py-4 rounded text-white font-medium w-full" 
         type="submit"
         value="Sign In" />
