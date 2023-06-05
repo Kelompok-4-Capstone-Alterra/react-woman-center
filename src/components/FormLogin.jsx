163a5bc771b6ba8be237defada362721b8fe4a60
unknown <wiraprathamaalvin@gmail.com>
2023-06-05 14:32:14 +0800

    chores: add calendar component

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -16,16 +16,6 @@ const FormLogin = () => {
      <form onSubmit={handleSubmit(onSubmit)}>
         <InputField name="username" label="Username" type="text" placeholder="johndoe" errors={errors} register={register}  />
         <InputField name="password" label="Password" type="password" placeholder="it's secret" errors={errors} register={register}  />
-        <Dropdown
-          control={control}
-          name="myDropdown"
-          label="Choose Sub-Menu"
-          placeholder="Choose"
-        >
-          <option label="option 1" value="option1"/>
-          <option label="option 2" value="option2"/>
-          <option label="option 3" value="option3"/>
-        </Dropdown>
       <input
         className="flex justify-center bg-primaryMain px-2 py-4 rounded text-white font-medium w-full" 
         type="submit"
