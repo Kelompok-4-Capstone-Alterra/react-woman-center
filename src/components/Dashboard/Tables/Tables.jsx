fdf1e1dd60a4fa490accafa952ff2e5d900ca03c
Nafibal <baliya363@gmail.com>
2023-06-19 07:47:32 +0700

    fix : resize table width

--- src/components/Dashboard/Tables/Tables.jsx
+++ src/components/Dashboard/Tables/Tables.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Tables = ({ children, scroll }) => {
   return (
     <div className="overflow-auto">
-      <table className={` ${scroll ? "w-[140%]" : "w-full"}  text-center`}>
+      <table className={` ${scroll ? "w-[120%]" : "w-full"}  text-center`}>
         {children}
       </table>
     </div>
