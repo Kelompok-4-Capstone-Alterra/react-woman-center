64eb99ae1eb4000cf9852d4e6e55d67782aff5e6
unknown <wiraprathamaalvin@gmail.com>
2023-06-21 00:47:03 +0800

    chores: fix add and update counselors

--- src/pages/UserCounselorPage.jsx
+++ src/pages/UserCounselorPage.jsx
@@ -22,7 +22,7 @@ import { Alert, MenuItem, Select, Skeleton, Snackbar } from "@mui/material";
 const { VITE_API_BASE_URL } = import.meta.env;
 
 const UserCounselorPage = () => {
-  const { control, getValues, register, handleSubmit, formState: { errors } } = useForm();
+  const { control, getValues, register, handleSubmit, setValue, formState: { errors } } = useForm();
   const [isCounselor, setIsCounselor] = useState(true);
   const [isAdd, setisAdd] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
@@ -33,6 +33,16 @@ const UserCounselorPage = () => {
   const [user, setUser] = useState(null);
   const [counselor, setCounselor] = useState(null);
   const [sortBy, setSortBy] = useState("newest");
+  const [showModalConfirmUser, setShowModalConfirmUser] = useState(false);
+  const [showModalConfirmCounselor, setShowModalConfirmCounselor] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [selectedCounselorId, setSelectedCounselorId] = useState(null);
+  const [isShowToast, setIsShowToast] = useState({
+    isOpen: false,
+    variant: "info",
+    duration: 5000,
+    message: "",
+  });
 
   const onSubmit = async (counselorData) => {
     const formData = new FormData();
@@ -59,6 +69,47 @@ const UserCounselorPage = () => {
           };
       
           const response = await axios(config);
+
+          handleAdd();
+          const counselorsData = await getAllCounselors();
+          setData(counselorsData);
+
+          return response.data.meta;
+      } catch (error) {
+        throw error.response.meta;
+      }
+  };
+
+  const updateCounselor = async (counselorData) => {
+    const formData = new FormData();
+        formData.append('name', counselorData.name);
+        formData.append('email', counselorData.email);
+        formData.append('username', counselorData.username);
+        formData.append('topic', counselorData.topic.value);
+        formData.append('description', counselorData.description);
+        formData.append('price', counselorData.price);
+        formData.append('profile_picture', selectedImage);
+
+        try {
+          const token = getAuthCookie();
+          const id = counselor.id;
+      
+          const config = {
+            method: "PUT",
+            baseURL: VITE_API_BASE_URL,
+            url: `/admin/counselors/${id}`,
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'multipart/form-data'
+            },
+            data: formData
+          };
+      
+          const response = await axios(config);
+
+          handleView();
+          const counselorsData = await getAllCounselors();
+          setData(counselorsData);
       
           return response.data.meta;
       } catch (error) {
@@ -95,7 +146,14 @@ const UserCounselorPage = () => {
   const deleteCounselor = async (counselorId) => {
     try {
       const response = await deleteCounselorById(counselorId);
-      getAllCounselors();
+      setIsShowToast({
+        ...isShowToast,
+        isOpen: true,
+        variant: "success",
+        message: response.message,
+      });
+      const counselorsData = await getAllCounselors();
+      setData(counselorsData);
     } catch (error) {
       console.error(error);
     }
@@ -104,39 +162,57 @@ const UserCounselorPage = () => {
   const deleteUser = async (userId) => {
     try {
       const response = await deleteUserById(userId);
-      getAllUsers();
+      setIsShowToast({
+        ...isShowToast,
+        isOpen: true,
+        variant: "success",
+        message: response.message,
+      });
+      const usersData = await getAllUsers();
+      setData(usersData);
     } catch (error) {
       console.error(error);
     }
   };
 
   const getUser = async (userId) => {
-    setisView(!isView);
     try {
       const userData = await getUserById(userId);
       setUser(userData);
     } catch (error) {
       console.error(error);
     }
+    setisView(!isView);
   };
 
   const getCounselor = async (userId) => {
-    setisView(!isView);
     try {
       const counselorData = await getCounselorById(userId);
       setCounselor(counselorData);
     } catch (error) {
       console.error(error);
     }
+    setisView(!isView);
   };
 
+  useEffect(() => {
+    if (counselor) {
+      setValue('image', counselor?.profile_picture);
+      setValue('id', counselor?.id);
+      setValue('name', counselor?.name);
+      setValue('email', counselor?.email);
+      setValue('username', counselor?.username);
+      setValue('topic', counselor?.topic);
+      setValue('description', counselor?.description);
+      setValue('price', counselor?.price);
+    }
+  }, [counselor, setValue]);
 
   const handleImageChange = (file) => {
     setSelectedImage(file);
     const fileURL = URL.createObjectURL(file);
     setImagePreview(fileURL);
   };
-
 
   const handleSelect = () => {
     const formData = getValues();
@@ -146,12 +222,41 @@ const UserCounselorPage = () => {
     console.log("Value: ", dropdownValue.value);
   };
 
-  const handleEdit = () =>  {
+  const handleAdd = () =>  {
     setisAdd(!isAdd);
   }
 
   const handleView = () =>  {
     setisView(!isView);
+  }
+
+  const openModalConfirmCounselor = (counselorId) => {
+    setSelectedCounselorId(counselorId);
+    setShowModalConfirmCounselor(true);
+  };
+
+  const handleShowModalConfirmCounselor= (showModal) => {
+    setShowModalConfirmCounselor(showModal);
+  };
+
+  const handleDeleteCounselor = (counselorId) => {
+    deleteCounselor(counselorId);
+    handleShowModalConfirmCounselor(false);
+  }
+
+  const openModalConfirmUser = (userId) => {
+    setSelectedUserId(userId);
+    setShowModalConfirmUser(true);
+  };
+
+  const handleShowModalConfirmUser = (showModal) => {
+    setShowModalConfirmUser(showModal);
+  };
+  
+
+  const handleDeleteUser = (userId) => {
+    deleteUser(userId);
+    handleShowModalConfirmUser(false);
   }
 
   const handleSearchCounselor= (event) => {
@@ -178,7 +283,7 @@ const UserCounselorPage = () => {
           </form>
           <div>
           {isCounselor && (
-          <ButtonPrimary className="flex items-center justify-center text-sm" onClick={handleEdit}>
+          <ButtonPrimary className="flex items-center justify-center text-sm" onClick={handleAdd}>
             <AddIcon /> Add Counselor
           </ButtonPrimary>
           )}
@@ -223,11 +328,6 @@ const UserCounselorPage = () => {
             </MenuItem>
             <MenuItem value="oldest">Oldest</MenuItem>
           </Select>
-          {/* <span className="text-base">Sort By</span>
-          <FilterDropdown>
-            <FilterDropdown.Option>Newest</FilterDropdown.Option>
-            <FilterDropdown.Option>Oldest</FilterDropdown.Option>
-          </FilterDropdown> */}
         </div>
     <TableContainer>
       <TableTitle title={`${isCounselor ? "Counselors" : "Users"}`}/>
@@ -255,18 +355,18 @@ const UserCounselorPage = () => {
           <TableBody>
           {isCounselor ? (
           data && Array.isArray(data) ? (
-            data.map((item) => (
-              <TableRow key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>{item.topic}</td>
+            data.map((counselor) => (
+              <TableRow key={counselor.id}>
+                <td>{counselor.id}</td>
+                <td>{counselor.name}</td>
+                <td>{counselor.username}</td>
+                <td>{counselor.email}</td>
+                <td>{counselor.topic}</td>
                 <td>
-                <ButtonPrimary onClick={() => getCounselor(item.id)}>View</ButtonPrimary>
+                <ButtonPrimary onClick={() => getCounselor(counselor.id)}>View</ButtonPrimary>
                 </td>
                 <td>
-                  <ButtonOutline onClick={() => deleteCounselor(item.id)}>Delete</ButtonOutline>
+                  <ButtonOutline onClick={() => openModalConfirmCounselor(counselor.id)}>Delete</ButtonOutline>
                 </td>
               </TableRow>
             ))
@@ -277,17 +377,17 @@ const UserCounselorPage = () => {
           )
         ) : (
           data && Array.isArray(data) ? (
-            data.map((item) => (
-              <TableRow key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
+            data.map((user) => (
+              <TableRow key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
                 <td>
-                  <ButtonPrimary onClick={() => getUser(item.id)}>View</ButtonPrimary>
+                  <ButtonPrimary onClick={() => getUser(user.id)}>View</ButtonPrimary>
                 </td>
                 <td>
-                  <ButtonOutline onClick={() => deleteUser(item.id)}>Delete</ButtonOutline>
+                  <ButtonOutline onClick={() => openModalConfirmUser(user.id)}>Delete</ButtonOutline>
                 </td>
               </TableRow>
             ))
@@ -301,7 +401,7 @@ const UserCounselorPage = () => {
         </Tables>
       </TableContainer>
     </div>
-    <Modal isOpen={isAdd} type={'addCounselor'}>
+    <Modal isOpen={isAdd} type={'addCounselor'} onClose={handleAdd}>
               <Modal.Title title={'New Counselor'} />
               <div>
               <form className="mb-3" onSubmit={handleSubmit(onSubmit)}>
@@ -324,40 +424,63 @@ const UserCounselorPage = () => {
               errors={errors}
               register={register}
             >
-              <option value="1" label="1. Self Development" />
-              <option value="2" label="2. Family/Relationship"  />
-              <option value="3" label="3. Spiritual"  />
-              <option value="4" label="4. Career"  />
-              <option value="5" label="5. Discrimination"  />
-              <option value="6" label="6. Sexual Abuse"  />
-              <option value="7" label="7. Couples"  />
-              <option value="8" label="8. Depression"  />
-              <option value="9" label="9. Mental Disorder"  />
-              <option value="10" label="10. Self Harming Behaviour"  />
+              <option value="1" label="Self Development" />
+              <option value="2" label="Family/Relationship"  />
+              <option value="3" label="Spiritual"  />
+              <option value="4" label="Career"  />
+              <option value="5" label="Discrimination"  />
+              <option value="6" label="Sexual Abuse"  />
+              <option value="7" label="Couples"  />
+              <option value="8" label="Depression"  />
+              <option value="9" label="Mental Disorder"  />
+              <option value="10" label="Self Harming Behaviour"  />
             </Dropdown>
               <InputField name="description" label="Description" type="text" placeholder="Ex : Counselor work to empower women to make positive changes" errors={errors} register={register}  />
               <InputField name="price" label="Counseling Price" type="number" placeholder="johndoe" errors={errors} register={register}  />
               <ButtonPrimary>+ Add New Counselor</ButtonPrimary>
               </form>
-              <ButtonOutline onClick={handleEdit}>Not Now</ButtonOutline>
+              <ButtonOutline onClick={handleAdd}>Not Now</ButtonOutline>
               </div>
       </Modal>
       {isCounselor ? (
-        <Modal isOpen={isView} type={'viewUpdateCounselor'}>
+        <Modal isOpen={isView} type={'viewUpdateCounselor'} onClose={handleView}>
               <Modal.Title title={'View & Update Counselor'} />
               <div>
               <form className="mb-3" onSubmit={handleSubmit(updateCounselor)}>
-              <ImageUploader>
+              <ImageUploader
+              icon={<AddIcon />}
+              name="image"
+              handleChange={handleImageChange}
+              errors={errors}
+              register={register}>
                 <ImageThumbnail src={counselor?.profile_picture}/>
               </ImageUploader>
-              <InputField name="username" label="ID" type="preview" value={counselor?.id} errors={errors} register={register}  />
-              <InputField name="username" label="Name" type="text" placeholder={counselor?.name} errors={errors} register={register}  />
-              <InputField name="username" label="Email" type="text" placeholder={counselor?.email} errors={errors} register={register}  />
-              <InputField name="username" label="Username" type="text" placeholder={counselor?.username} errors={errors} register={register}  />
-              <InputField name="username" label="Topic" type="text" placeholder={counselor?.topic} errors={errors} register={register}  />
-              <InputField name="username" label="Description" type="text" placeholder={counselor?.description} errors={errors} register={register}  />
-              <InputField name="username" label="Counseling Price" type="number" placeholder={counselor?.price} errors={errors} register={register}  />
-              <ButtonPrimary onClick={handleView}>Save</ButtonPrimary>
+              <InputField name="id" label="ID" type="text" errors={errors} register={register} disabled={true} />
+              <InputField name="name" label="Name" type="text"  errors={errors} register={register}  />
+              <InputField name="email" label="Email" type="text" errors={errors} register={register}  />
+              <InputField name="username" label="Username" type="text" errors={errors} register={register}  />
+              <Dropdown
+              control={control}
+              name="topic"
+              label="Category"
+              placeholder={counselor?.topic}
+              errors={errors}
+              register={register}
+            >
+              <option value="1" label="Self Development" />
+              <option value="2" label="Family/Relationship"  />
+              <option value="3" label="Spiritual"  />
+              <option value="4" label="Career"  />
+              <option value="5" label="Discrimination"  />
+              <option value="6" label="Sexual Abuse"  />
+              <option value="7" label="Couples"  />
+              <option value="8" label="Depression"  />
+              <option value="9" label="Mental Disorder"  />
+              <option value="10" label="Self Harming Behaviour"  />
+            </Dropdown>
+              <InputField name="description" label="Description" type="text" value={counselor?.description} errors={errors} register={register}  />
+              <InputField name="price" label="Counseling Price" type="number" value={counselor?.price} errors={errors} register={register}  />
+              <ButtonPrimary>Save</ButtonPrimary>
               </form>
               <ButtonOutline onClick={handleView}>Close</ButtonOutline>
               </div>
@@ -380,10 +503,22 @@ const UserCounselorPage = () => {
               </div>
        </Modal>
             )}
-      <ModalConfirm
-      isConfirm={isDelete}
-      messages="Are you sure you want to delete this item?"
-      />
+            {showModalConfirmUser && (
+            <ModalConfirm
+              onSure={() => handleDeleteUser(selectedUserId)}
+              onClose={handleShowModalConfirmUser}
+              isConfirm={showModalConfirmUser}
+              messages="Are you sure want to delete this item?"
+            />
+          )}
+          {showModalConfirmCounselor && (
+            <ModalConfirm
+              onSure={() => handleDeleteCounselor(selectedCounselorId)}
+              onClose={handleShowModalConfirmCounselor}
+              isConfirm={showModalConfirmCounselor}
+              messages="Are you sure want to delete this item?"
+            />
+          )}
     </>
   )
 }
