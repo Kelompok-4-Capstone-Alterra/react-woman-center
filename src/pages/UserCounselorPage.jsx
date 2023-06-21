fd091a70325270a5ab6d4f16ce4e450bebd90e78
unknown <wiraprathamaalvin@gmail.com>
2023-06-21 22:05:30 +0800

    chores : fix ui

--- src/pages/UserCounselorPage.jsx
+++ src/pages/UserCounselorPage.jsx
@@ -304,46 +304,6 @@ const UserCounselorPage = () => {
           )}
           </div>
     </div>
-    <div className="flex justify-end items-center gap-4 mb-4">
-          <span className="text-base">Sort By</span>
-          <Select
-            value={sortBy}
-            onChange={(event) => setSortBy(event.target.value)}
-            sx={{
-              ".MuiSelect-select": {
-                padding: "0.325rem 0.75rem",
-              },
-              ".MuiOutlinedInput-notchedOutline": {
-                borderColor: "#9E9494 !important",
-                borderWidth: "1px",
-              },
-            }}
-            MenuProps={{
-              sx: {
-                "&& .Mui-selected": {
-                  backgroundColor: "#AF1582 !important",
-                  color: "#FFF",
-                },
-                "&& .Mui-selected:hover": {
-                  backgroundColor: "#954E80 !important",
-                },
-              },
-            }}
-          >
-            <MenuItem
-              value="newest"
-              sx={{
-                "&:checked": {
-                  backgroundColor: "#AF1582 !important",
-                  color: "#FFF",
-                },
-              }}
-            >
-              Newest
-            </MenuItem>
-            <MenuItem value="oldest">Oldest</MenuItem>
-          </Select>
-        </div>
     <TableContainer>
       <TableTitle title={`${isCounselor ? "Counselors" : "Users"}`}/>
         <Tables>
