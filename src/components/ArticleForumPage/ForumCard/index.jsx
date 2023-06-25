1bfb0a0da254426a56daaf7a0be0126992c2f655
Abdulloh <abdullohfahmi45@gmail.com>
2023-06-25 14:07:52 +0700

    chore: add view link on forum and set skeleton loading for user info

--- src/components/ArticleForumPage/ForumCard/index.jsx
+++ src/components/ArticleForumPage/ForumCard/index.jsx
@@ -10,16 +10,26 @@ import axios from "axios";
 import { getAuthCookie } from "../../../utils/cookies";
 import moment from "moment";
 import avatarDefault from "../../../assets/forum/avatar-default.png";
+import Modal from "../../Modal";
+import InputField from "../../InputField";
+import { useForm } from "react-hook-form";
+import { Skeleton } from "@mui/material";
 
 const { VITE_API_BASE_URL } = import.meta.env;
 
 const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
   const backgroundStyle = {
     background:
       "conic-gradient(from 180deg at 50% 50%, #DAD29C -120deg, #F9DAD9 33.75deg, #DAD29C 240deg, #F9DAD9 393.75deg)",
   };
 
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
   const [isShowModalConfirm, setIsShowModalConfirm] = useState(false);
+  const [isShowModalEdit, setIsShowModalEdit] = useState(false);
   const dispatch = useDispatch();
   const { user_id, category, link, topic, member, created_at } = payloads;
   const [user, setUser] = useState({});
@@ -31,6 +41,8 @@ const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
 
   const fetchUserById = async () => {
     const token = getAuthCookie();
+
+    setIsLoadingUser(true);
 
     try {
       const response = await axios.get(
@@ -45,6 +57,8 @@ const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
     } catch (error) {
       console.error(error);
     }
+
+    setIsLoadingUser(false);
   };
 
   const handleOpenModalConfirm = () => {
@@ -53,6 +67,10 @@ const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
 
   const handleShowModalConfirm = (showModal) => {
     setIsShowModalConfirm(showModal);
+  };
+
+  const handleShowModalEdit = (showModal) => {
+    setIsShowModalEdit(showModal);
   };
 
   const handleDeleteForum = () => {
@@ -69,20 +87,27 @@ const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
       <div className="flex flex-row w-full">
         <div className="flex justify-between w-full mb-4">
           <div className="flex flex-row items-center">
-            <img
-              src={user?.profile_picture}
-              className="w-[40px] h-[40px] rounded-full"
-              onError={handleImageError}
-            />
-
+            {isLoadingUser ? (
+              <Skeleton variant="circular" width={40} height={40} />
+            ) : (
+              <img
+                src={user.profile_picture}
+                className="w-[40px] h-[40px] rounded-full"
+                onError={handleImageError}
+              />
+            )}
             <div className="flex flex-col ml-2">
-              <p className="font-medium text-sm capitalize">{user?.name}</p>
+              {isLoadingUser ? (
+                <Skeleton variant="text" sx={{ fontSize: "0.875rem" }} />
+              ) : (
+                <p className="font-medium text-sm capitalize">{user.name}</p>
+              )}
               <p className="font-medium text-xs text-slate-500 capitalize">
                 {category}
               </p>
             </div>
           </div>
-          <div className="flex flex-row justify-end items-center">
+          <div className="flex flex-row justify-end items-center gap-2">
             <p className="font-normal text-xs flex items-center gap-x-1">
               {member} <PeopleAltOutlinedIcon />
             </p>
@@ -94,16 +119,47 @@ const ForumCard = ({ openEdit, openModalComment, payloads, deleteForum }) => {
       </div>
       <div className="flex flex-row h-[36px] mb-6 gap-x-4 justify-end">
         <div className="columns">
-          <ButtonPrimary className="h-full w-[100px] flex justify-center items-center">
-            <VisibilityIcon /> View
+          <ButtonPrimary
+            className="h-full min-w-[125px] flex justify-center items-center"
+            onClick={() => handleShowModalEdit(true)}
+          >
+            <VisibilityIcon className="mr-1" />
+            <span className="text-[0.75rem] font-medium">View Link</span>
           </ButtonPrimary>
+          {isShowModalEdit && (
+            <Modal
+              isOpen={isShowModalEdit}
+              type="link"
+              onClose={handleShowModalEdit}
+            >
+              <h2 className="font-medium text-[22px] text-neutralHigh capitalize mb-4">
+                {topic.length > 35 ? `${topic.substring(0, 35)}...` : topic}
+              </h2>
+              <InputField
+                type="preview"
+                id="forumLink"
+                name={"forumLink"}
+                label={"Forum’s Link"}
+                placeholder={"Your Link"}
+                value={link}
+                disabled
+              />
+              <ButtonPrimary
+                onClick={() => handleShowModalEdit(false)}
+                className="h-fit w-full px-3 py-3 flex items-center justify-center"
+              >
+                <span className="text-[1rem] font-medium">Close</span>
+              </ButtonPrimary>
+            </Modal>
+          )}
         </div>
         <div className="columns">
           <ButtonOutline
-            className="h-full w-[119px] flex justify-center items-center"
+            className="h-full min-w-[110px] flex justify-center items-center"
             onClick={handleOpenModalConfirm}
           >
-            <DeleteIcon /> Delete
+            <DeleteIcon className="mr-1" />
+            <span className="text-[0.75rem] font-medium">Delete</span>
           </ButtonOutline>
           {isShowModalConfirm && (
             <ModalConfirm
