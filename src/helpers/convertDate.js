d2bb5e5d50b6e02e0837c0b5e0881689277df4eb
Abdulloh <abdullohfahmi45@gmail.com>
2023-06-25 15:25:48 +0700

    chore: add params separator

--- src/helpers/convertDate.js
+++ src/helpers/convertDate.js
@@ -1,11 +1,11 @@
-export const convertDate = (dateString) => {
+export const convertDate = (dateString, separator = "-") => {
   const date = new Date(dateString);
 
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
 
-  const formattedDate = `${year}-${month}-${day}`;
+  const formattedDate = `${year}${separator}${month}${separator}${day}`;
 
   return formattedDate;
 };
