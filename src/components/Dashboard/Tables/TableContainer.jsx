a0dc6bbfad5521b26fc5ff48d8c531592608736a
Nafibal <baliya363@gmail.com>
2023-06-25 13:01:07 +0700

    style: remove padding on TableContainer component

--- src/components/Dashboard/Tables/TableContainer.jsx
+++ src/components/Dashboard/Tables/TableContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TableContainer = ({ children }) => {
-  return <div className="max-w-full p-4">{children}</div>;
+  return <div className="max-w-full py-4">{children}</div>;
 };
 
 export default TableContainer;
