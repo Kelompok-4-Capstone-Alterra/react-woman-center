d8c335c73dc1520dff13b2fcd7c7b1b6a21bcd1c
Abdulloh <abdullohfahmi45@gmail.com>
2023-06-26 21:05:39 +0700

    refactor: refactoring popup component

--- src/components/Dashboard/Popup.jsx
+++ src/components/Dashboard/Popup.jsx
@@ -2,50 +2,30 @@ import React from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import AnnouncementIcon from "@mui/icons-material/Announcement";
 
-const Popup = ({ message, success, failed, isOpen }) => {
+const Popup = ({ message, isSuccess, isOpen }) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed w-[100vw] h-[100vh] z-10 inset-0 flex flex-col items-center overflow-auto">
+        <div className="fixed w-[100vw] h-[100vh] z-[998] inset-0 flex flex-col items-center overflow-auto">
           <div className="fixed bg-black opacity-50 w-[100vw] h-[100vh] inset-0"></div>
-          <div className="z-10 h-full flex justify-center items-center">
-            <div className=" w-[250px] h-[125px] justify-center items-center flex flex-col rounded shadow-md op bg-white">
-              {success && (
+          <div className="z-[999] h-full flex justify-center items-center">
+            <div className="text-center justify-center items-center gap-4 flex flex-col rounded shadow-md op bg-white py-8 px-14">
+              {isSuccess && (
                 <CheckCircleIcon
-                  fontSize="large"
-                  className="mb-2 text-successMain"
+                  className="text-successMain"
+                  sx={{ fontSize: "50px" }}
                 />
               )}
-              {failed && (
+              {!isSuccess && (
                 <AnnouncementIcon
-                  fontSize="large"
-                  className="mb-2 text-dangerMain"
+                  className="text-dangerMain"
+                  sx={{ fontSize: "50px" }}
                 />
               )}
-              {message}
+              <p className="font-medium capitalize-first">{message}</p>
             </div>
           </div>
         </div>
-        // <div className="fixed w-[100vw] h-[100vh] z-10 inset-0 flex flex-col items-center overflow-auto">
-        //   <div className="fixed bg-black opacity-50 w-[100vw] h-[100vh] inset-0"></div>
-        //   <div className="fixed bg-black opacity-50 w-[100vw] h-[100vh] inset-0 flex justify-center items-center">
-        //     <div className=" w-[210px] h-[105px] justify-center items-center flex flex-col rounded shadow-md bg-white">
-        //       {success && (
-        //         <CheckCircleIcon
-        //           fontSize="large"
-        //           className="mb-2 text-successMain"
-        //         />
-        //       )}
-        //       {failed && (
-        //         <AnnouncementIcon
-        //           fontSize="large"
-        //           className="mb-2 text-dangerMain"
-        //         />
-        //       )}
-        //       {message}
-        //     </div>
-        //   </div>
-        // </div>
       )}
     </>
   );
